f94ccc22f52ecd107ab42470135f401d18bbf775
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-14 19:06:06 -0400

    testing

--- app/controllers/artist_instruments_controller.rb
+++ app/controllers/artist_instruments_controller.rb
@@ -1,18 +1,18 @@
 class ArtistInstrumentsController < ApplicationController
 
 	def new
-		@artist_instruments = ArtistInstruments.new
+		@artist_instrument = ArtistInstrument.new
 		@artists = Artist.all
 		@instruments = Instrument.all
 	end
 
 	def create
-		@artist_instruments = ArtistInstruments.new(params.require(:artist_instruments).permit(:artist_id, :instrument_id))
-		if @artist_instruments.save
-			redirect_to artist_path(@artist_instruments.artist.id)
+		@artist_instrument = ArtistInstrument.new(params.require(:artist_instrument).permit(:artist_id, :instrument_id))
+		if @artist_instrument.save
+			redirect_to artist_path(@artist_instrument.artist.id)
 		else
-			flash[:errors] = @artist_instruments.errors.full_messages
-			redirect_to new_artist_instruments_path
+			flash[:errors] = @artist_instrument.errors.full_messages
+			redirect_to new_artist_instrument_path
 		end
 	end
 
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,25 @@
 class ArtistsController < ApplicationController
+
+	def index
+		@artists = Artist.all
+	end
+
+	def show
+		@artist = Artist.find(params[:id])
+	end
+
+	def new
+		@artist = Artist.new
+	end
+
+	def create
+		@artist = Artist.new(params.require(:artist).permit(:name, :age, :title))
+		if @artist.save
+			redirect_to artist_path(@artist)
+		else
+			flash[:errors] = @artist.errors.full_messages
+			redirect_to new_artist_path
+		end
+	end
+	 
 end
